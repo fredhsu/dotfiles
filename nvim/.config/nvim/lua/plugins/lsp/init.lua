404873736e6572798f9f221031133e1f8e272501
fredhsu <fredlhsu@gmail.com>
2023-06-14 14:27:00 -0700

    update

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,23 +1,41 @@
 return {
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			build = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
+        -- Autocompletion
+        {
+            'hrsh7th/nvim-cmp',
+            event = 'InsertEnter',
+            dependencies = {
+                {'L3MON4D3/LuaSnip'},
+            },
+            config = function ()
+                require('lsp-zero.cmp').extend()
+                local cmp = require('cmp')
+                local cmp_action = require('lsp-zero.cmp').action()
+                cmp.setup({
+                    mapping = {
+                        ['<CR>'] = cmp.mapping.confirm({select = true}),
+                        -- Ctrl+Space to trigger completion menu
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        -- Navigate between snippet placeholder
+                        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                    }
+                })
+            end
+        },     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
 }
 
 }
